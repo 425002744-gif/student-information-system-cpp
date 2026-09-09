2fc4731d1b167d96c3456a074bcc7be0151326a9
Andrei Lowell Canlas <425002744@ntc.edu.ph>
2026-09-09 15:04:01 +0800

    Add student ID and email

--- student-information-system.cpp
+++ student-information-system.cpp
@@ -8,8 +8,10 @@ int main() {
     cout << endl;
     cout << "Name: Andrei Canlas" << endl;
     cout << "Course: BSIT" << endl;
-    cout << "Year Level: 2" << endl;
-    cout << endl;
+   cout << "Year Level: 2" << endl;
+cout << "Student ID: 425002744" << endl;
+cout << "Email: 425002744@ntc.edu.ph" << endl;
+cout << endl;
     cout << "Welcome to the system!" << endl;
 
     return 0;
